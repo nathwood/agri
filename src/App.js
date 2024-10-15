e30cdf6d8a403277a5c328c1a0b819b3ece94476
blue460 <66220429+blue460@users.noreply.github.com>
2024-10-15 13:23:17 +0800

    connect weight input and include state values

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import WeightInput from './components/WeightInput';
 
 function App() {
+
+  // State variables
+  const [weights, setWeights] = useState([]);
+
+  // Function to add weight
+  const addWeight = (newWeight) => {
+    const updatedWeights = [...weights, newWeight];
+    setWeights(updatedWeights);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
+
+      <h1>AgriSpark Weight Tracker</h1>
+      <WeightInput addWeight = {addWeight} />
+
     </div>
   );
 }
