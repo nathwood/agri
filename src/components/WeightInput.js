555968b52f02df24851669709c64a0395ff881af
blue460 <66220429+blue460@users.noreply.github.com>
2024-10-15 13:08:33 +0800

    weight input

---
+++ src/components/WeightInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const WeightInput = ({ addWeight }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formattedValue = parseFloat(inputValue).toFixed(2);
+
+        if (!isNaN(formattedValue) && inputValue !== '') {
+            addWeight(formattedValue);
+            setInputValue('');
+        } else {
+            alert('Please enter a valid number');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type = "number"
+                step = "0.01"
+                value = {inputValue}
+                onChange = {(e) => setInputValue(e.target.value)}
+                placeholder = "Enter weight (e.g. 50.12)"
+            />
+            <button type = "submit">
+                Add
+            </button>
+        </form>
+    )
+}
+
+export default WeightInput;
