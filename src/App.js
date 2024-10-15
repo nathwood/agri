0570ef7eb9433438425f8453861bfe881a0499f8
blue460 <66220429+blue460@users.noreply.github.com>
2024-10-15 15:21:44 +0800

    setup local storage

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ function App() {
   // State variables
   const [weights, setWeights] = useState([]);
 
+  // Local storage
+  useEffect( () => {
+    const savedWeights = JSON.parse(localStorage.getItem('weights')) || [];
+    setWeights(savedWeights);
+  }, []);
+
   // Function to add weight
   const addWeight = (newWeight) => {
     const updatedWeights = [...weights, newWeight];
     setWeights(updatedWeights);
+    localStorage.setItem('weights', JSON.stringify(updatedWeights));
   }
 
   return (
     <div className="App">
 
       <h1>AgriSpark Weight Tracker</h1>
+      <h2>User: Ahmad</h2>
       <WeightInput addWeight = {addWeight} />
 
     </div>
